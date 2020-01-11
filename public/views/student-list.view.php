cda1c89e1a9312f9da1b2c905a80724c54fb14d2
Alexandru Coarna <coarna.alexandru@gmail.com>
2020-01-11 18:29:56 +0200

    +html sql

--- public/views/student-list.view.php
+++ public/views/student-list.view.php
@@ -4,12 +4,40 @@
 <div class="main">
     <table id="student-list">
         <tr>
+            <th>No.</th>
             <th>First Name</th>
             <th>Last Name</th>
+            <th>Phone No.</th>
+            <th>Email</th>
+            <th>Country</th>
+            <th>City</th>
         </tr>
         <tr>
+            <td>1</td>
             <td>Coarna</td>
             <td>Alexandru</td>
+            <td>0769889441</td>
+            <td>coarna.alexandru@gmail.com</td>
+            <td>Romania</td>
+            <td>Galati</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Coarna</td>
+            <td>Alexandru</td>
+            <td>0769889441</td>
+            <td>coarna.alexandru@gmail.com</td>
+            <td>Romania</td>
+            <td>Galati</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Coarna</td>
+            <td>Alexandru</td>
+            <td>0769889441</td>
+            <td>coarna.alexandru@gmail.com</td>
+            <td>Romania</td>
+            <td>Galati</td>
         </tr>
     </table>
 </div>
