4cfe61a4b85a58da55330d172ff89a4c83c3a49c
Alexandru Coarna <coarna.alexandru@gmail.com>
2020-01-11 17:55:54 +0200

    +html sql

---
+++ docker-compose/tpi/dump/seed.sql
@@ -0,0 +1,14 @@
+alter database tpi
+    character set utf8mb4
+    collate utf8mb4_unicode_ci;
+
+create table if not exists student
+(
+    id           int primary key auto_increment,
+    first_name   char(50),
+    last_name    char(50),
+    phone_numner char(11),
+    email        char(50),
+    country      char(30),
+    city         char(30)
+);
--- public/views/add-student.view.php
+++ public/views/add-student.view.php
@@ -5,8 +5,22 @@
         <form id="add-student">
             <label for="fname">First Name</label>
             <input type="text" id="fname" name="firstname" placeholder="First Name">
+
             <label for="lname">Last Name</label>
             <input type="text" id="lname" name="lastname" placeholder="Last Name">
+
+            <label for="pnumber">Phone Number</label>
+            <input type="text" id="pnumber" name="lastname" placeholder="Phone Number">
+
+            <label for="email">Email</label>
+            <input type="text" id="email" name="lastname" placeholder="Email">
+
+            <label for="country">Country</label>
+            <input type="text" id="country" name="lastname" placeholder="Country">
+
+            <label for="city">City</label>
+            <input type="text" id="city" name="lastname" placeholder="City">
+            
             <input type="submit" value="Add Student">
         </form>
     </div>
